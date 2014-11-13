f251d31334a627efeb9832893758a632c902c0be
Danny DeBelius <danny.debelius@gmail.com>
2014-11-13 15:17:38 -0500

    Poster landing page is back

--- www/js/app.js
+++ www/js/app.js
@@ -19,6 +19,7 @@ var onDocumentLoad = function(e) {
     $shareModal.on('shown.bs.modal', onShareModalShown);
     $shareModal.on('hidden.bs.modal', onShareModalHidden);
     $goButton.on('click', onGoButtonClick);
+    $(window).on('resize', onWindowResize);
 
     // configure ZeroClipboard on share panel
     ZeroClipboard.config({ swfPath: 'js/lib/ZeroClipboard.swf' });
@@ -110,7 +111,17 @@ var showNewSong = function(e) {
  */
 var onGoButtonClick = function(e) {
     $('.current-song, .player, .playlist-filters').fadeIn();
-    $(this).fadeOut();
+
+    $('html, body').animate({
+        scrollTop: $('.current-song').offset().top
+    }, 2000);
+}
+
+/*
+ * Fade in the next song of the playlist
+ */
+var onWindowResize = function(e) {
+    $('.landing').css('height', $(window).height());
 }
 
 
