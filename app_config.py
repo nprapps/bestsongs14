f5c24af30d535822bb096367133ffdd0e9d8a3b0
Christopher Groskopf <staringmonkey@gmail.com>
2014-12-04 15:28:33 -0600

    Add first four new headshots. #201.

--- app_config.py
+++ app_config.py
@@ -143,15 +143,10 @@ REVIEWER_TAGS = [
 ]
 
 REVIEWER_IMAGES = {
-    'Lars Gotrich': 'lars.jpg',
-    'Ann Powers': 'ann.jpg',
-    'Otis Hart': 'otis.jpg',
-    'Jacob Ganz': 'jacob.jpg',
-    'Sami Yenigun': 'sami.jpg',
-    'Stephen Thompson': 'sthompson.jpg',
-    'Robin Hilton': 'robin.jpg',
-    'Jason King': 'jason.jpg',
-    'Bob Boilen': 'bob.jpg'
+    'Jason Bentley': 'jason-bentley.jpg',
+    'Rita Houston': 'rita-houst.jpg',
+    'David Dye': 'david-dye.jpg',
+    'Kevin Cole': 'kevin-cole.jpg'
 }
 
 SHUFFLE_AUDIO_INTRO = 'assets/shuffle-intro.mp3'
