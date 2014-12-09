4c95b566fc0708bbe0f6ec560ba53722b73e3643
Danny DeBelius <danny.debelius@gmail.com>
2014-12-09 11:34:07 -0500

    Fix reviewer tape transitions

--- www/js/app.js
+++ www/js/app.js
@@ -825,10 +825,13 @@ var hideWelcome  = function() {
         complete: function() {
             $landing.velocity('fadeOut', {
                 delay: 2000,
-                duration: 1000
+                duration: 1000,
+                complete: function() {
+                    $('.poster').removeClass('shrink').attr('style','');
+                }
             });
         }
-    })
+    });
 
     onWelcome = false;
 
@@ -861,7 +864,7 @@ var onContinueButtonClick = function(e) {
     e.preventDefault();
     buildPlaylist();
     updateTagDisplay();
-    hideWelcome();
+    $landing.velocity('fadeOut');
     playNextSong();
 }
 
